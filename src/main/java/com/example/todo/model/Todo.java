fc26c29d714625e4584cabc4bf0475271d5ea190
DanNicolaeBoca <dan.nicolae.boca01@gmail.com>
2025-07-01 22:17:09 +0300

    Todo class and enum

---
+++ src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,33 @@
+package com.example.todo.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Document(collection = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    private String description;
+
+    private TodoStatus status = TodoStatus.TODO;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+}
