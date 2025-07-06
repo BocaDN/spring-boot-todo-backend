6af3f363b7697fe3fc64388dacfe81f6d0581896
DanNicolaeBoca <dan.nicolae.boca01@gmail.com>
2025-07-07 01:39:29 +0300

    Changes TodoController class

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Tag(name = "Todos", description = "Todo API")
 @RestController
 @RequestMapping("/todo")
